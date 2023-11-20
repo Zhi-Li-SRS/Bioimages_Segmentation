a88e5078d7a613052d53513bc9e606636fed3123
Zhi-Li-SRS <zhl026@ucsd.edu>
2023-11-20 14:00:06 -0800

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="cellSegmentation",
+    version="0.0.0",
+    author="Zhi Li",
+    author_email="zhl026@ucsd.edu",
+    packages=find_packages(),
+    install_requires=[],
+)
